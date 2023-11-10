0afe75d5c1558f55d2c663628e490d2e163b48f6
Patrick Robrecht <mail@patrick-robrecht.de>
2023-11-10 21:49:10 +0100

    Use portavice/laravel-pint-config

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -67,7 +67,7 @@ class LoginRequest extends FormRequest
      */
     public function ensureIsNotRateLimited(): void
     {
-        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+        if (!RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
             return;
         }
 

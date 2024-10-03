a59ed02915c960bfe4093aa66465d4fcc2fe79b8
Patrick Robrecht <patrickrobrecht@users.noreply.github.com>
2024-10-03 16:02:00 +0200

    Add command to check whether all documents are saved under the specified path (#48)

---
+++ app/Console/Commands/CheckDocumentsCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Document;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\Storage;
+
+class CheckDocumentsCommand extends Command
+{
+    protected $signature = 'app:check-documents';
+    protected $description = 'Check whether all documents are saved under the specified path';
+
+    /** @var string[] */
+    private array $missingDocumentPaths = [];
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        Document::query()
+            ->orderBy('id')
+            ->chunk(100, fn (Collection $documents) => $this->checkDocuments($documents));
+
+        if (count($this->missingDocumentPaths) === 0) {
+            $this->info('All documents ok.');
+            return self::SUCCESS;
+        }
+
+        $this->error(sprintf('%d documents are missing.', count($this->missingDocumentPaths)));
+        return self::FAILURE;
+    }
+
+    /**
+     * @param Collection<Document> $documents
+     */
+    public function checkDocuments(Collection $documents): void
+    {
+        foreach ($documents as $document) {
+            if (!Storage::exists($document->path)) {
+                $this->missingDocumentPaths[] = $document->path;
+                $this->error(sprintf('%s missing', $document->path));
+            }
+        }
+    }
+}
